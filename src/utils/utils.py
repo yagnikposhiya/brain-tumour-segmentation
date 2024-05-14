1aac616e24bc5bb59b17744f2d638a83c0c6e44f
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-05-14 20:17:52 +0530

    removed cmap parameter from imshow() in-built function to print plots into color; add plt.show(): to display the plots; add code for changing matplotlib backend

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,9 @@ author: Yagnik Poshiya
 github: @yagnikposhiya
 organization: NEUROWORK Research Labs
 """
+import matplotlib
+matplotlib.use('TkAgg')  # or 'Qt5Agg' or any other backend that supports interactive display
+# by default backend: FigureCanvasAgg
 
 import os
 import nibabel as nib
@@ -28,7 +31,7 @@ def showAllTypesOfImages(trainset_path:str, image_name:list) -> None:
     image_mask = nib.load(os.path.join(trainset_path,image_name[4])).get_fdata() # load mask type image data
 
     print("Shape of a flair image: {}".format(image_flair.shape))  # shape of the flair image
-    print("Shape of a T1 image: {}".fomat(image_t1.shape)) # shape of the T1 image
+    print("Shape of a T1 image: {}".format(image_t1.shape)) # shape of the T1 image
     print("Shape of a T1CE image: {}".format(image_t1ce.shape)) # shape of the T1CE image
     print("Shape of a T2 image: {}".format(image_t2.shape)) # shape of the T2 image
     print("Shape of a Mask image: {}".format(image_mask.shape)) # shape of the Mask image
@@ -37,17 +40,19 @@ def showAllTypesOfImages(trainset_path:str, image_name:list) -> None:
 
     SLICE_W = 25 # unknown variable
 
-    ax1.imshow(image_flair[:,:,image_flair.shape[0]//2-SLICE_W], cmap='gray') # visualize flair image
+    ax1.imshow(image_flair[:,:,image_flair.shape[0]//2-SLICE_W]) # visualize flair image
     ax1.set_title('Flair Image') # set title of a figure
 
-    ax2.imshow(image_t1[:,:,image_t1.shape[0]//2-SLICE_W], cmap='gray') # visualize t1 image
+    ax2.imshow(image_t1[:,:,image_t1.shape[0]//2-SLICE_W]) # visualize t1 image
     ax2.set_title('T1 Image') # set title of a figure
 
-    ax3.imshow(image_t1ce[:,:,image_t1ce.shape[0]//2-SLICE_W], cmap='gray') # visualize t1ce image
+    ax3.imshow(image_t1ce[:,:,image_t1ce.shape[0]//2-SLICE_W]) # visualize t1ce image
     ax3.set_title('T1CE Image') # set title of a figure
 
-    ax4.imshow(image_t2[:,:,image_t2.shape[0]//2-SLICE_W], cmap='gray') # visualize t2 image
+    ax4.imshow(image_t2[:,:,image_t2.shape[0]//2-SLICE_W]) # visualize t2 image
     ax4.set_title('T2 Image') # set title of a figure
 
-    ax5.imshow(image_mask[:,:,image_mask.shape[0]//2-SLICE_W], cmap='gray') # visualize mask image
+    ax5.imshow(image_mask[:,:,image_mask.shape[0]//2-SLICE_W]) # visualize mask image
     ax5.set_title('Mask Image') # set title of a figure
+
+    plt.show() # display the plots
