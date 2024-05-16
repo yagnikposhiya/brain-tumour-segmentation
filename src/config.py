57c08d4005b3e7adf366c85a2a36457bd652e1df
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-05-16 16:59:24 +0530

    added trainset_path, validset_path, and path for images used for testing purpose

--- src/config.py
+++ src/config.py
@@ -11,3 +11,13 @@ class Config():
         
         # current working directory
         self.CWD = os.getcwd() # get current working directory
+
+        # training and validation dataset path
+        self.TRAINSET_PATH = os.path.join(self.CWD,'data/raw/') # set training directory path
+        self.VALIDSET_PATH = '' # set validation directory path
+        self.TRAIN_IMAGE_PATH =['unstructured/BraTS20_Training_001_flair.nii',
+                                'unstructured/BraTS20_Training_001_t1.nii',
+                                'unstructured/BraTS20_Training_001_t1ce.nii',
+                                'unstructured/BraTS20_Training_001_t2.nii',
+                                'unstructured/BraTS20_Training_001_seg.nii'] # set path for any images available in the train directory
+        
