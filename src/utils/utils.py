afd585d0e77e9710b2522362f5899a88e2d59322
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-05-30 20:54:23 +0530

    added option in user choice list i.e. MobileNetV3UNet (large) without SE Block

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -439,7 +439,8 @@ def available_models() -> None:
               'MobileNetV3UNet (small)',
               'MobileNetV3UNet (large)',
               'Cascaded MobileNetV3UNet (large)',
-              'BoxUNet'] # list of available models
+              'BoxUNet',
+              'MobileNetV3UNet (large) without SE Block'] # list of available models
     
     print("Select any one neural network architecture from the list given below")
     for i in range(len(models)):
