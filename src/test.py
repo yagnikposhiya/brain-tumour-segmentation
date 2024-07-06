dfdc0555e2249dc2dc302d4323310ef7de98ecc4
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-07-07 04:25:07 +0530

    added mobilenetv3 small and large without seblock architecture in user choice list

--- src/test.py
+++ src/test.py
@@ -14,7 +14,9 @@ from nn_arch.mobilenetv2 import MobileNetV2UNet
 from nn_arch.mobilenetv3_small import MobileNetV3SmallUNet
 from nn_arch.mobilenetv3_large import MobileNetV3LargeUNet
 from nn_arch.cascaded_mobilenetv3_large import CascadedMobileNetV3LargeUNet
-from utils.utils import load_saved_model, available_models, prepareImageForInference, groundTruthVSPredicted_AllClasses
+from nn_arch.mobilenetv3_large_without_SEblock import MobileNetV3LargeUNet_Without_SEBlock
+from nn_arch.mobilenetv3_small_without_SEblock import MobileNetV3SmallUNet_Without_SEBlock
+from utils.utils import load_saved_model, available_models, prepareImageForInference, groundTruthVSPredicted_AllClasses, available_optimizers
 
 
 if __name__=='__main__':
@@ -26,7 +28,8 @@ if __name__=='__main__':
     print("-------------------------------------------------")
 
     avail_models, user_choice = available_models() # get user choice for available models for training
-    print(f"- You have seleted {avail_models[user_choice]}")
+    avail_optim, user_choice_optim = available_optimizers() # get user choice for available optimizers for training
+    print(f"- You have selected {avail_optim[user_choice_optim]} optimizer with {avail_models[user_choice]} neural network architecture.")
 
     if user_choice == 0:
         model_class = UNet # create a normal standard unet model
@@ -42,8 +45,12 @@ if __name__=='__main__':
         model_class = CascadedMobileNetV3LargeUNet # create Cascaded MobileNetV3-Large model
     elif user_choice == 6:
         model_class = BoxUNet # create BoxUNet model
+    elif user_choice == 7:
+        model_class = MobileNetV3LargeUNet_Without_SEBlock # create MobileNetV3-Large architecture without SE Block
+    elif user_choice == 8:
+        model_class = MobileNetV3SmallUNet_Without_SEBlock # create MobileNetV3-Small architecture without SE Block
 
-    loaded_model = load_saved_model(model_class=model_class,num_classes=config.NUM_CLASSES,learning_rate=config.LEARNING_RATE) # load saved trained model
+    loaded_model = load_saved_model(model_class=model_class,num_classes=config.NUM_CLASSES,learning_rate=config.LEARNING_RATE, optimizer=avail_optim[user_choice_optim]) # load saved trained model
     print("Saved model is loaded successfully.")
 
     print("-------------------------------------------------")
